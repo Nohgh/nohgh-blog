0e6f4bf18aa4af9c584e24d3af57f796f0d40911
노 기 훈 <kiki01111@naver.com>
2026-01-02 02:49:39 +0900

    fix markdownToHtml for codeblock styling

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,7 +1,19 @@
-import { remark } from "remark";
-import html from "remark-html";
+import { unified } from "unified";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import rehypeStringify from "rehype-stringify";
+import rehypePrettyCode from "rehype-pretty-code";
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown);
-  return result.toString();
+  const file = await unified()
+    .use(remarkParse)
+    .use(remarkRehype)
+    .use(rehypePrettyCode, {
+      theme: "github-dark",
+      keepBackground: true,
+    })
+    .use(rehypeStringify)
+    .process(markdown);
+
+  return String(file);
 }
