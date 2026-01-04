c51b81428e5caad9e6aa87e657be76864c12f2db
노 기 훈 <kiki01111@naver.com>
2026-01-05 00:25:12 +0900

    feat: mobile에서 빠르게 확인을 위한 qr tool 간단하게 개발

---
+++ __tools__/ip-qr.mjs
@@ -0,0 +1,16 @@
+import { exec } from 'child_process'
+import localIpUrl from 'local-ip-url'
+import qr from 'qrcode-terminal'
+
+const port = Number(process.env.PORT) || 3000
+const ip = localIpUrl('public')
+
+const url = `http://${ip}:${port}`
+console.log(`\n Local Network URL: ${url}\n`)
+
+qr.generate(url, { small: true })
+
+const nextDev = exec(`next dev --turbopack -p ${port}`)
+
+nextDev.stdout.pipe(process.stdout)
+nextDev.stderr.pipe(process.stderr)
