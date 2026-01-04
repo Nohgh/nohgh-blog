cace45566f2029dd93b819928505c4192f536f56
노 기 훈 <kiki01111@naver.com>
2026-01-04 23:39:41 +0900

    design: header md이전까지 blur 적용

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -3,7 +3,7 @@ import { ThemeSwitcher } from './theme-switcher'
 
 const Header = () => {
   return (
-    <header className="font-gowun sticky top-0 py-2 pr-12 z-40 h-[40px] w-full flex flex-row justify-end gap-6 text-base 2xl:text-lg">
+    <header className="font-gowun sticky top-0 py-2 pr-12 z-40 h-[40px] w-full flex flex-row justify-end gap-6 text-base 2xl:text-lg backdrop-blur-sm md:backdrop-blur-0">
       <Link href="/" className="hover:underline">
         홈
       </Link>
