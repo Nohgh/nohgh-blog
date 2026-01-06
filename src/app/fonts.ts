7457826419ea044898c9c01e26c75e4a9f0d5334
노 기 훈 <kiki01111@naver.com>
2026-01-06 14:46:11 +0900

    opt: font 최적화

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -3,12 +3,12 @@ import localFont from 'next/font/local'
 export const Gowun = localFont({
   src: [
     {
-      path: '../../public/fonts/GowunBatang-Regular.woff',
+      path: '../../public/fonts/GowunBatang-Regular.woff2',
       weight: '400',
       style: 'normal',
     },
     {
-      path: '../../public/fonts/GowunBatang-Bold.woff',
+      path: '../../public/fonts/GowunBatang-Bold.woff2',
       weight: '700',
       style: 'normal',
     },
