89ac98e9fa381facc5e9cce427e095638017932f
노 기 훈 <kiki01111@naver.com>
2026-01-08 03:37:17 +0900

    chore: manifest 제거, base url 수정

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,4 +3,4 @@ export const HOME_OG_IMAGE_URL = '/assets/blog/gihoon-blog-op-image.png'
 export const PROFILE_IMAGE_URL = '/assets/about/gihoon-profile.JPG'
 
 // urls
-export const SITE_URL = 'https://gihoon-blog.vercel.app'
+export const SITE_URL = 'https://gihoon.me'
