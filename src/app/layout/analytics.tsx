790ca3c5ef646ad44f48022a1c225851ae14268f
노 기 훈 <kiki01111@naver.com>
2026-01-26 20:45:51 +0900

    chore: add  vercel analytics

--- src/app/layout/analytics.tsx
+++ src/app/layout/analytics.tsx
@@ -1,8 +1,10 @@
 import { GoogleAnalytics } from '@next/third-parties/google'
+import { Analytics as VercelAnalytics } from '@vercel/analytics/next'
 
 export function Analytics() {
   return (
     <>
+      <VercelAnalytics />
       <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GAID!} />
     </>
   )
